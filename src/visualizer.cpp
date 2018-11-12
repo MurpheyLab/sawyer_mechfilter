a1f97aacd4fe3ca2c3bc19487df247e8dd99d369
kt-fitz <fitzsi28@gmail.com>
2018-11-12 16:02:16 -0600

    rvix markers not working

--- src/visualizer.cpp
+++ src/visualizer.cpp
@@ -3,27 +3,55 @@
 #include "geometry_msgs/TransformStamped.h"
 #include <visualization_msgs/Marker.h>
 #include <visualization_msgs/MarkerArray.h>
+#include "sawyer_humcpp/mdasys.h"
 //#include "std_msgs/Float32MultiArray.h"
 //#include <sstream>
 //#include <iostream>
 //#include <armadillo>
 //#include <random>
-
+using namespace std;
 class VisualCP{
   ros::Subscriber mda_sub;
   ros::Publisher marker_pub;
-  ros::NodeHandle* nh;  
+  ros::NodeHandle* nh;
+  //visualization_msgs::MarkerArray markerset;
+  //Mass Marker
+  visualization_msgs::Marker mass;
+    
+   visualization_msgs::Marker marker;std::cout<<"wtf"<<std::endl;
+   //marker.header.frame_id = "base_link";
+   //marker.header.stamp = ros::Time();
+    //mass.header.frame_id = "cart";
+    /*mass.header.stamp=ros::Time();
+    mass.ns = ros::this_node::getNamespace()+"sim_world";
+    mass.id = 0;
+    mass.type = visualization_msgs::Marker::SPHERE;
+    mass.action=visualization_msgs::Marker::ADD;
+    mass.scale.x=0.13 ;mass.scale.y=0.13;mass.scale.z=0.13;
+    mass.color.a=1.0;mass.color.r=1.0;mass.color.g=1.0;mass.color.b=1.0;*/
+  //Mass Marker
+  /*visualization_msgs::Marker link;
+    link.header.frame_id = "link";
+    link.header.stamp=mass.header.stamp;
+    link.ns = ros::this_node::getNamespace()+"sim_world";
+    link.id = 1;
+    link.type = visualization_msgs::Marker::LINE_STRIP;
+    link.action=visualization_msgs::Marker::ADD;
+    link.scale.x=0.13 ;mass.scale.y=0.13;mass.scale.z=0.13;
+    link.color.a=1.0;mass.color.r=1.0;mass.color.g=1.0;mass.color.b=1.0;
+  */
+  
   public:
     VisualCP(ros::NodeHandle* _nh){
     nh=_nh;
     ROS_INFO("Starting RVIZ");
     //setup publishers & subscribers
-    //marker_pub = nh->advertise<sawyer_humcpp::mdasys>("mda_topic", 5);  
-    //interactCommand = nh->advertise<intera_core_msgs::InteractionControlCommand>("/robot/limb/right/interaction_control_command",1);
-    //cursor_state = nh->subscribe("/robot/limb/right/endpoint_state",5,&ImpedeSimulator::update_state,this);
-    //end_acc = nh->subscribe("/robot/accelerometer/right_accelerometer/state",5,&ImpedeSimulator::calc_input,this);
+    marker_pub = nh->advertise<visualization_msgs::MarkerArray>("visualization_marker_array", 5);  
+    mda_sub = nh->subscribe("/mda_topic",5,&VisualCP::update_state,this);
     };
     void timercall(const ros::TimerEvent& event){
+    };
+    void update_state(const sawyer_humcpp::mdasys& state){
     };
     
 };
