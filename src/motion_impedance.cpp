1441105f998a70319e6354e86c65614e674e1302
kt-fitz <fitzsi28@gmail.com>
2019-06-12 14:03:19 -0500

    set up move to neutral

--- src/motion_impedance.cpp
+++ src/motion_impedance.cpp
@@ -31,8 +31,9 @@ SERVICES:
 #include <intera_motion_msgs/MotionCommandAction.h>
 #include <actionlib/client/simple_action_client.h>
 
-const double DT=1./4.;
-const double Nwpt = 200.;
+
+const double DT=0.5;
+const double Nwpt = 1.;
 
 typedef actionlib::SimpleActionClient<intera_motion_msgs::MotionCommandAction> Client;
 using namespace std;
@@ -51,6 +52,7 @@ class MotionSim{
   //ros::Publisher mda_pub;
   ros::Subscriber cursor_state;
   ros::Subscriber end_acc;
+  ros::Subscriber traj_result;
   //sawyer_humcpp::mdasys currstate;
   tf2::Quaternion q_acc, q_ep,q_ep_force;
   bool initcon=false;
@@ -58,6 +60,7 @@ class MotionSim{
   
   string joint_names[7] = {"right_j0", "right_j1", "right_j2", "right_j3", "right_j4", "right_j5", "right_j6"};
   intera_motion_msgs::MotionCommandGoal motiongoal;
+  intera_motion_msgs::MotionCommandGoal motionstop; 
   Client* ac = new Client("/motion/motion_command",true);
   geometry_msgs::Pose pose_init;
   geometry_msgs::Pose pose_now;
@@ -72,44 +75,16 @@ class MotionSim{
     interactCommand = nh->advertise<intera_core_msgs::InteractionControlCommand>("/robot/limb/right/interaction_control_command",1);
     cursor_state = nh->subscribe("/robot/limb/right/endpoint_state",5,&MotionSim::update_state,this);
     end_acc = nh->subscribe("/robot/accelerometer/right_accelerometer/state",5,&MotionSim::calc_input,this);
+    traj_result = nh->subscribe("/motion/motion_command/result",5,&MotionSim::check_traj,this);
       
     //setup ros action server client
-    //ac("/motion/motion_command",true);
-    //ROS_INFO("Waiting for action server to start.");
     ac->waitForServer();
     ROS_INFO("Action server started.");
-    /*ROS_INFO("Moving to Start Position");
-    intera_motion_msgs::Trajectory traj;
-    traj.joint_names = {"right_j0", "right_j1", "right_j2", "right_j3", "right_j4", "right_j5", "right_j6"};
-    intera_motion_msgs::Waypoint wpt;
-    wpt.active_endpoint = "right_hand";
-    wpt.pose.header.stamp = ros::Time::now();
-    wpt.pose.header.seq=1;
-    wpt.pose.header.frame_id = "base";
-    wpt.options.max_joint_speed_ratio = 1.0;
-    wpt.options.joint_tolerances = {0.1,0.1,0.1,0.1,0.1,0.1,0.1};
-    wpt.options.max_linear_speed = 2.0;
-    wpt.options.max_linear_accel = 2.0;
-    wpt.options.max_rotational_speed=1.57;
-    wpt.options.max_rotational_accel = 1.57;
-    wpt.options.corner_distance = 0.5;
-    wpt.pose.pose.position.x = 0.6; wpt.pose.pose.position.y = 0.1; wpt.pose.pose.position.z = 0.2;
-    wpt.pose.pose.orientation.x = 0.707; wpt.pose.pose.orientation.y = 0.707;
-    wpt.pose.pose.orientation.z = 0.; wpt.pose.pose.orientation.w = 0.;
-    traj.waypoints.push_back(wpt);
-    traj.trajectory_options.interpolation_type = "CARTESIAN";
-    traj.trajectory_options.interaction_control=false;
-    traj.trajectory_options.nso_start_offset_allowed = true;//set to false for 'small' motions
-    traj.trajectory_options.end_time = t0 + ros::Duration(0.5);
-    traj.trajectory_options.path_interpolation_step = 1./100.;
-    motiongoal.trajectory = traj;
-    ac->sendGoal(motiongoal);
-    ROS_INFO("In Start Position");
-    */
-    };
+   };
   
   //set up timer callback fxn
   void timercall(const ros::TimerEvent& event){
+    while(ros::Time::now()-t0<tcurr+ros::Duration(DT)){};
     tcurr = ros::Time::now() - t0;
     if(initcon==false) {return;};
     motiongoal.command = "start";
@@ -122,34 +97,42 @@ class MotionSim{
     wpt_next.pose.header.frame_id = "base";
     wpt_next.options.max_joint_speed_ratio = 1.0;
     wpt_next.options.joint_tolerances = {0.5,0.5,0.5,0.5,0.5,0.5,0.5};
-    wpt_next.options.max_linear_speed = 5.0;
-    wpt_next.options.max_linear_accel = 5.0;
-    wpt_next.options.max_rotational_speed=3.0;
-    wpt_next.options.max_rotational_accel = 3.0;
+    wpt_next.options.max_linear_speed = 50.0;
+    wpt_next.options.max_linear_accel = 50.0;
+    wpt_next.options.max_rotational_speed=10.0;
+    wpt_next.options.max_rotational_accel = 10.0;
     wpt_next.options.corner_distance = 0.5;
     for (int i = 1; i<=Nwpt; i++){
       wpt_next.pose.pose = pose_init;
-      wpt_next.pose.pose.position.x = pose_init.position.x - 0.05*sin(tcurr.toSec()+(i*DT/Nwpt));
-      wpt_next.pose.pose.position.y = pose_init.position.y + 0.1*(cos(tcurr.toSec()+(i*DT/Nwpt))-1);
+      wpt_next.pose.pose.position.x = pose_init.position.x - 0.05*sin(tcurr.toSec()/2+(i*DT/Nwpt));
+      wpt_next.pose.pose.position.y = pose_init.position.y + 0.2*(cos(tcurr.toSec()/2+(i*DT/Nwpt))-1);
       traj.waypoints.push_back(wpt_next);
     }
     traj.trajectory_options.interpolation_type = "CARTESIAN";
     traj.trajectory_options.interaction_control=false;//true; interact_options(true);
     //traj.trajectory_options.interaction_params = interactopt;
     traj.trajectory_options.nso_start_offset_allowed = true;//set to false for 'small' motions
-    traj.trajectory_options.tracking_options.goal_time_tolerance = DT;
-    traj.trajectory_options.end_time = t0 + tcurr+ ros::Duration(DT);
-    traj.trajectory_options.path_interpolation_step = DT;
+    traj.trajectory_options.tracking_options.goal_time_tolerance = DT/10;
+    traj.trajectory_options.end_time = ros::Time::now()+ros::Duration(DT);
+    traj.trajectory_options.path_interpolation_step = 0.;
     motiongoal.trajectory = traj;
+    motionstop.command = "stop";
+    ac->sendGoal(motionstop);
     ac->sendGoal(motiongoal);
+    
    };
   
   //state update from end effector
   void update_state(const intera_core_msgs::EndpointState& state){
       pose_now = state.pose;
-      if(initcon==false){
+      double eta = 0.02;
+      if(abs(pose_now.position.x-0.6)<eta and abs(pose_now.position.y-0.1)<eta 
+        and abs(pose_now.position.z-0.2)<eta
+        and abs(pose_now.orientation.x-0.707)<eta and abs(pose_now.orientation.y-0.707)<eta
+        and abs(pose_now.orientation.z)<0.01 and abs(pose_now.orientation.w)<0.01){
         pose_init = state.pose;
-        initcon=true;ROS_INFO("Initcon set");
+        if(initcon = false){ROS_INFO("Initcon set");};
+        initcon=true;
       };      
            
   };
@@ -158,6 +141,14 @@ class MotionSim{
     tf2::Quaternion q_a_temp;
     q_a_temp.setValue(imu.linear_acceleration.x,imu.linear_acceleration.y,imu.linear_acceleration.z,0.0);
     q_acc = q_ep*q_a_temp*q_ep.inverse();   
+    
+  };
+    
+  void check_traj(const intera_motion_msgs::MotionCommandActionResult& res){
+    if(!res.result.result){
+        ROS_INFO("Tracjectory Failed");
+        ROS_INFO("%s\n",res.result.errorId.c_str());
+    };  
     
   };
   
@@ -185,7 +176,40 @@ class MotionSim{
     interactopt.disable_damping_in_force_control = true;
     interactopt.disable_reference_resetting = false;
     interactopt.rotations_for_constrained_zeroG = false;
-  }; 
+  };
+  
+  void set_neutral(){
+    ROS_INFO("Moving to Start Position");
+    motiongoal.command = "start";
+    intera_motion_msgs::Trajectory traj;
+    traj.joint_names = {"right_j0", "right_j1", "right_j2", "right_j3", "right_j4", "right_j5", "right_j6"};
+    intera_motion_msgs::Waypoint wpt;
+    wpt.active_endpoint = "right_hand";
+    wpt.pose.header.stamp = ros::Time::now();
+    wpt.pose.header.seq=1;
+    wpt.pose.header.frame_id = "base";
+    wpt.options.max_joint_speed_ratio = 1.0;
+    wpt.options.joint_tolerances = {0.1,0.1,0.1,0.1,0.1,0.1,0.1};
+    wpt.options.max_linear_speed = 2.0;
+    wpt.options.max_linear_accel = 2.0;
+    wpt.options.max_rotational_speed=1.57;
+    wpt.options.max_rotational_accel = 1.57;
+    wpt.options.corner_distance = 0.5;
+    wpt.pose.pose.position.x = 0.6; wpt.pose.pose.position.y = 0.1; wpt.pose.pose.position.z = 0.2;
+    wpt.pose.pose.orientation.x = 0.707; wpt.pose.pose.orientation.y = 0.707;
+    wpt.pose.pose.orientation.z = 0.; wpt.pose.pose.orientation.w = 0.;
+    traj.waypoints.push_back(wpt);
+    traj.trajectory_options.interpolation_type = "CARTESIAN";
+    traj.trajectory_options.interaction_control=false;
+    traj.trajectory_options.nso_start_offset_allowed = true;//set to false for 'small' motions
+    traj.trajectory_options.end_time = t0 + ros::Duration(5.0);
+    traj.trajectory_options.path_interpolation_step = 1./100.;
+    motiongoal.trajectory = traj;
+    ac->sendGoal(motiongoal);
+    while(ros::Time::now().toSec()<traj.trajectory_options.end_time.toSec()){};
+    ROS_INFO("In Start Position");
+     
+  };
         
 };
     
@@ -195,9 +219,11 @@ int main(int argc, char **argv){
   ros::init(argc, argv, "motion_test");
   ros::NodeHandle n;
   MotionSim sim(&n);
+  sim.set_neutral();
   ros::Timer timer = n.createTimer(ros::Duration(DT), &MotionSim::timercall, &sim);
   ros::spin();
   //try putting shutdown commands here?
+  ROS_INFO("Shutting down now");
   //intera_core_msgs::InteractionControlCommand clearimpedance;
   //n.interactCommand.publish()
  return 0;
