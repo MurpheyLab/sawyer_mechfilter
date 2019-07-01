7db79ef0720574c24dfb0f8e30045b3f44629417
kt-fitz <fitzsi28@gmail.com>
2019-07-01 11:37:26 -0500

    node created for move to neutral

---
+++ src/gohome.cpp
@@ -0,0 +1,169 @@
+/*
+Kathleen Fitzsimons
+
+This node runs a timer that uses the current state of the cursor to drive the trep simulation. 
+The position of the pendulum is also published.
+
+SUBSCRIBERS:
+    - cursor_state (nact3d/cursor)
+
+PUBLISHERS:
+    - mass_point (PointStamped)
+    - visualization_marker_array (MarkerArray)
+    - cursor_bias (Float32MultiArray)
+    - cursor_dyn (nact3d/cursor_dyn)
+    - trep_sys (mda_act3d/mdasys)
+    - act3d_read (Bool)
+    - acceptance (Bool)
+
+SERVICES:
+*/
+#include<typeinfo>
+#include <ros/ros.h>
+#include <iostream>
+#include <ros/console.h>
+#include <geometry_msgs/Pose.h>
+#include <tf2_geometry_msgs/tf2_geometry_msgs.h>
+#include <intera_core_msgs/InteractionControlCommand.h>
+#include <intera_core_msgs/EndpointState.h>
+#include <sensor_msgs/Imu.h>
+//For specifying a motion command
+#include <intera_motion_msgs/MotionCommandAction.h>
+#include <actionlib/client/simple_action_client.h>
+
+
+const double DT=0.5;
+const double Nwpt = 1.;
+
+typedef actionlib::SimpleActionClient<intera_motion_msgs::MotionCommandAction> Client;
+using namespace std;
+
+class Homing{
+  ros::Time t0 = ros::Time::now();
+  ros::NodeHandle* nh;
+  ros::Subscriber cursor_state;
+  ros::Subscriber traj_result;
+  bool initcon=false;
+    
+  string joint_names[7] = {"right_j0", "right_j1", "right_j2", "right_j3", "right_j4", "right_j5", "right_j6"};
+  intera_motion_msgs::MotionCommandGoal motiongoal;
+  intera_motion_msgs::MotionCommandGoal motionstop; 
+  Client* ac = new Client("/motion/motion_command",true);
+  geometry_msgs::Pose pose_init;
+  geometry_msgs::Pose pose_now;
+  
+  public:
+  Homing(ros::NodeHandle* _nh){
+    nh=_nh;
+    ROS_INFO("Creating Move to Neutral class");
+    //setup publishers & subscribers
+    cursor_state = nh->subscribe("/robot/limb/right/endpoint_state",5,&Homing::update_state,this);
+    traj_result = nh->subscribe("/motion/motion_command/result",5,&Homing::check_traj,this);
+      
+    //setup ros action server client
+    ac->waitForServer();
+    ROS_INFO("Action server started.");
+   };
+  
+  //set up timer callback fxn
+  void timercall(const ros::TimerEvent& event){
+    if(initcon == true){ROS_INFO("Arm in Home Position");};
+    
+   };
+  
+  //state update from end effector
+  void update_state(const intera_core_msgs::EndpointState& state){
+      pose_now = state.pose;
+      double eta = 0.02;
+      if(abs(pose_now.position.x-0.6)<eta and abs(pose_now.position.y-0.1)<eta 
+        and abs(pose_now.position.z-0.2)<eta
+        and abs(pose_now.orientation.x-0.707)<eta and abs(pose_now.orientation.y-0.707)<eta
+        and abs(pose_now.orientation.z)<0.01 and abs(pose_now.orientation.w)<0.01){
+        pose_init = state.pose;
+        if(initcon == false){ROS_INFO("Initcon set");};
+        initcon=true;
+      };      
+           
+  };
+  
+  void check_traj(const intera_motion_msgs::MotionCommandActionResult& res){
+    if(!res.result.result){
+        ROS_INFO("Tracjectory Failed");
+        ROS_INFO("%s\n",res.result.errorId.c_str());
+    };  
+    
+  };
+  
+  //setting the impedance of the interaction options message
+  /*void interact_options(bool reject){
+    interactopt.header.stamp = ros::Time::now();
+    interactopt.header.seq=1;
+    interactopt.header.frame_id = "base";
+    interactopt.interaction_control_active = true;
+    interactopt.interaction_control_mode = {1,3,1,1,1,1};
+    interactopt.K_impedance = {20,20,1300,1000,1000,1000};
+    interactopt.max_impedance = {true,false,true,true,true,true};
+    interactopt.D_impedance = {0,0,8.,0,2,2};
+    interactopt.K_nullspace = {0.,10.,10.,0.,0.,0.,0.};
+    interactopt.force_command = {300.,300.,0.,0.,0.,0.};
+    interactopt.interaction_frame.position.x = 0;
+    interactopt.interaction_frame.position.y =0;
+    interactopt.interaction_frame.position.z  =0;
+    interactopt.interaction_frame.orientation.x = 0;
+    interactopt.interaction_frame.orientation.y = 0;
+    interactopt.interaction_frame.orientation.z = 0;
+    interactopt.interaction_frame.orientation.w = 1;
+    interactopt.endpoint_name ="right_hand";
+    interactopt.in_endpoint_frame = false;
+    interactopt.disable_damping_in_force_control = true;
+    interactopt.disable_reference_resetting = false;
+    interactopt.rotations_for_constrained_zeroG = false;
+  };*/
+  
+  void set_neutral(){
+    ROS_INFO("Moving to Start Position");
+    motiongoal.command = "start";
+    intera_motion_msgs::Trajectory traj;
+    traj.joint_names = {"right_j0", "right_j1", "right_j2", "right_j3", "right_j4", "right_j5", "right_j6"};
+    intera_motion_msgs::Waypoint wpt;
+    wpt.active_endpoint = "right_hand";
+    wpt.pose.header.stamp = ros::Time::now();
+    wpt.pose.header.seq=1;
+    wpt.pose.header.frame_id = "base";
+    wpt.options.max_joint_speed_ratio = 1.0;
+    wpt.options.joint_tolerances = {0.1,0.1,0.1,0.1,0.1,0.1,0.1};
+    wpt.options.max_linear_speed = 2.0;
+    wpt.options.max_linear_accel = 2.0;
+    wpt.options.max_rotational_speed=1.57;
+    wpt.options.max_rotational_accel = 1.57;
+    wpt.options.corner_distance = 0.5;
+    wpt.pose.pose.position.x = 0.6; wpt.pose.pose.position.y = 0.1; wpt.pose.pose.position.z = 0.2;
+    wpt.pose.pose.orientation.x = 0.707; wpt.pose.pose.orientation.y = 0.707;
+    wpt.pose.pose.orientation.z = 0.; wpt.pose.pose.orientation.w = 0.;
+    wpt.joint_positions = {-0.1,-0.9,0.,1.8,0.,0.7,3.2};
+    traj.waypoints.push_back(wpt);
+    traj.trajectory_options.interpolation_type = "CARTESIAN";
+    traj.trajectory_options.interaction_control=false;
+    traj.trajectory_options.nso_start_offset_allowed = true;//set to false for 'small' motions
+    traj.trajectory_options.end_time = t0 + ros::Duration(5.0);
+    traj.trajectory_options.path_interpolation_step = 1./100.;
+    motiongoal.trajectory = traj;
+    ac->sendGoal(motiongoal);
+    while(ros::Time::now().toSec()<traj.trajectory_options.end_time.toSec()){};
+    ROS_INFO("In Start Position");
+     
+  };
+        
+};
+    
+int main(int argc, char **argv){
+  /*Run the main loop, by instatiating a System class, and then
+    calling ros::spin*/
+  ros::init(argc, argv, "gohome");
+  ros::NodeHandle n;
+  Homing sim(&n);
+  sim.set_neutral();
+  ros::Timer timer = n.createTimer(ros::Duration(DT), &Homing::timercall, &sim);
+  ros::spin();
+return 0;
+};
