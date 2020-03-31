ee65daa9e38d58b82468865a5a43e993826f2072
kt-fitz <fitzsi28@gmail.com>
2020-03-31 13:41:48 -0500

    fixed search!

--- src/imgwalls.py
+++ src/imgwalls.py
@@ -6,10 +6,10 @@ THRESHOLD = 130
 class imagewalls:
     def __init__(self,imageName):
         self.img = cv2.imread(imageName,cv2.IMREAD_GRAYSCALE)
-        self.width = self.img.shape[1]
+        self.width = self.img.shape[1]; #print self.width
         self.height = self.img.shape[0]
         
-    def pixelcheck(self,x,y):
+    def pixelcheck(self,y,x):
         black = False
         if(x<=0 or x>=self.width or y<=0 or y>=self.height):
             do=[]#do nothing
@@ -18,14 +18,14 @@ class imagewalls:
         return black
     
     def euclidist (self,x,y,xt,yt):
-        d=((x-xt)**2+(y-yt)**2)**0.5
+        d=((x-xt)**2.+(y-yt)**2.)**0.5
         return d
     
     def findnearest(self,x,y):
-        nearpixel = [0,0,0]
+        nearpixel = [0.,0.,0.]
         j=1
         found = False
-        while(found==False and j<= 200.):#self.width):
+        while(found==False and j<=self.width):
             dist =self.width*np.ones(8*j)
             pixels = np.zeros((8*j,2))
             for m in range(-j,j+1):
@@ -55,7 +55,7 @@ class imagewalls:
             nearpixel[0]=pixels[minindex,0]
             nearpixel[1]=pixels[minindex,1]  
             j=j+1
-        print np.where(nearpixel[2])
+        #print np.where(nearpixel[2])[0]
         return nearpixel
     
 imgName="apple.png";
